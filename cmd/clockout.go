0b86e216943c9bb37a65e437bbea7ebabf655795
sgash708 <sugash7081023@gmail.com>
2022-11-01 09:16:58 +0900

    chore: delete coment

--- cmd/clockout.go
+++ cmd/clockout.go
@@ -7,13 +7,7 @@ import (
 
 var clockoutCmd = &cobra.Command{
 	Use:   "clockout",
-	Short: "clock out",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "clock out in a certain site",
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := application.RunClockOut(); err != nil {
 			panic(err)
